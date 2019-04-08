9d361cf6b77a0d1bdf64099710ca5d5355098ddd
Xavier Klausener <xkl@reyl.rgrpdom.com>
2019-04-08 11:48:25 +0200

    v1.0

--- cli.js
+++ cli.js
@@ -1,43 +1,17 @@
 #!/usr/bin/env node
-'use strict';
-
-const path = require('path');
-const http = require('http');
-const request = require('request');
-const url = require('url');
-const fs = require('fs');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const openapi_json_schema_extractor_1 = require("./openapi-json-schema-extractor");
 const args = process.argv;
-
 async function main() {
     let inputKeyIndex = args.indexOf('--input');
     let outputKeyIndex = args.indexOf('--output');
     if (inputKeyIndex >= 0 && outputKeyIndex >= 0) {
-        let input = args[inputKeyIndex + 1];
-        let output = args[outputKeyIndex + 1];
-        if (input && output) {
-            if (await fs.exists(output))
-                await fs.rmdir(output);
-            await fs.mkdir(output);
-
-            let openapiFilename = input;
-            if (url.parse(input)) {
-                openapiFilename = path.join(output, 'openapi.json');
-                request(input).pipe(fs.createWriteStream(openapiFilename));
-            }
-            else {
-                if (!fs.existsSync(openapiFilename))
-                    throw new Error('--input filename ' + openapiFilename + ' doesn\'t exist');
-                await processInput(openapiFilename);
-            }
-        }
+        let openapiUrlOrFilename = args[inputKeyIndex + 1];
+        let outputDirectory = args[outputKeyIndex + 1];
+        openapi_json_schema_extractor_1.downloadAndExtractJsonSchemas(openapiUrlOrFilename, outputDirectory);
     }
 }
-
-async function processInput(openapiFilename, outputDirectory) {
-    console.log('Processing openapi file');
-    const stdout = await execa.stdout('openapi-generator', ['generate', '-i', openapiFilename, '-g', 'typescript-angular', '-o', outputDirectory, '-additional-properties', 'ngVersion=7.0.0']);
-    console.log('stdout openapi-generator: ' + stdout);
-}
-
-main();
+exports.main = main;
+main().catch(r => console.error(r));
+//# sourceMappingURL=cli.js.map
